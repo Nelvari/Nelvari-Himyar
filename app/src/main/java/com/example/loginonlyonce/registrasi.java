64c4696be31f0f64c7afc1df2dbf7507ba91f83a
muhsecha <muhammadsecha2@gmail.com>
2019-09-10 14:30:08 +0700

    change

--- app/src/main/java/com/example/loginonlyonce/registrasi.java
+++ app/src/main/java/com/example/loginonlyonce/registrasi.java
@@ -3,12 +3,62 @@ package com.example.loginonlyonce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONObject;
+
 
 public class registrasi extends AppCompatActivity {
+
+    EditText txtusername;
+    EditText notelp;
+    EditText email;
+    EditText txtpassword;
+    EditText konfirmpass;
+    Button   btnbuat;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrasi);
+
+        txtusername = findViewById(R.id.txtusername);
+        notelp = findViewById(R.id.notelp);
+        email = findViewById(R.id.email);
+        txtpassword = findViewById(R.id.txtpassword);
+        konfirmpass = findViewById(R.id.konfirmpass);
+        Button btnbuat = findViewById(R.id.btnbuat);
+
+
+
+        AndroidNetworking.post("http://api-ppdb.smkrus.com/api/v1/login")
+                .addBodyParameter("username", "txtusername")
+                .addBodyParameter("password", "txtpassword")
+                .addBodyParameter("role", "txtpassword")
+                .setTag("test")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("null", "onResponse: ");
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+
+                    }
+                });
+
+
     }
 }
