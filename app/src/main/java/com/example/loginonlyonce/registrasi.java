0bc676aa59b22de0a8d6dc7f7c1e19a86ad4da89
hasanwijaya <hasan@bitmessage.ch>
2019-09-10 12:07:10 +0700

    push

--- app/src/main/java/com/example/loginonlyonce/registrasi.java
+++ app/src/main/java/com/example/loginonlyonce/registrasi.java
@@ -1,14 +1,32 @@
 package com.example.loginonlyonce;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class registrasi extends AppCompatActivity {
+
+    Button btnBuat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrasi);
+
+        btnBuat = findViewById(R.id.btnbuat);
+
+        btnBuat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(registrasi.this, Login.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
