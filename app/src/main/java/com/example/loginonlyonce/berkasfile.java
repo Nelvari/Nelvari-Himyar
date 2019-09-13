62e1a3f3f45962e3f1f06da133104fa546a491c7
DeaAnandita <dea2018rplrus2@gmail.com>
2019-09-13 14:17:04 +0700

    push

---
+++ app/src/main/java/com/example/loginonlyonce/berkasfile.java
@@ -0,0 +1,14 @@
+package com.example.loginonlyonce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class berkasfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_berkasfile);
+    }
+}
