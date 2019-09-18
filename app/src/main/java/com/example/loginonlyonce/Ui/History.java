73e261b638087d6b033130c61ac3b5e028a3167f
hasanwijaya <hasan@bitmessage.ch>
2019-09-18 16:41:18 +0700

    push

--- app/src/main/java/com/example/loginonlyonce/Ui/History.java
+++ app/src/main/java/com/example/loginonlyonce/Ui/History.java
@@ -1,8 +1,10 @@
-package com.example.loginonlyonce;
+package com.example.loginonlyonce.Ui;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.loginonlyonce.R;
 
 public class History extends AppCompatActivity {
 
@@ -10,7 +12,6 @@ public class History extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-
 
     }
 
