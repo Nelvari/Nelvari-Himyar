c5991d95ed7c5bec1ea67fa820cb90c7df581b33
DeaAnandita <dea2018rplrus2@gmail.com>
2019-09-23 14:57:55 +0700

    push

--- app/src/main/java/com/example/loginonlyonce/Berkas_Isi.java
+++ app/src/main/java/com/example/loginonlyonce/Berkas_Isi.java
@@ -1,6 +1,7 @@
 package com.example.loginonlyonce;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,11 @@ public class Berkas_Isi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_berkas__isi);
+
+        Toolbar toolbar = findViewById(R.id.toolbarBerkasFile);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Berkas");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
